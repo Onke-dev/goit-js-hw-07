52a2279b450bc1973ce4616bc49e8903d63162b3
Nick <angertina777@ukr.net>
2026-02-16 23:18:58 +0200

    done task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const body = document.querySelector("body");
+const colors = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+btn.addEventListener("click", handleButton);
+
+function handleButton(e) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colors.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
