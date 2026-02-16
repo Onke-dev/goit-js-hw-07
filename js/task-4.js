ff159edb277cda51bbfd525462158a0fbc5fcbe0
Nick <angertina777@ukr.net>
2026-02-16 22:47:23 +0200

    done task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const formLogin = document.querySelector(".login-form");
+formLogin.addEventListener("submit", handleSubmit);
 
+function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email.trim() === "" || password.trim() === "") {
+      return alert(`All form fields must be filled in`);
+    }
+    console.log(email, password);
+    form.reset();
+}
